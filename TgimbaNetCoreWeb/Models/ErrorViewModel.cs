2a87359fdebd808640179d386f10f86b77541d9b
Eric Helin <ehelin@felpsis.net>
2021-02-15 14:26:35 -0600

    Add project files.

---
+++ TgimbaNetCoreWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TgimbaNetCoreWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
