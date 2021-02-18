5cad8a7755c4a80a4ae657d6e00deadc46375267
Eric Helin <erichelinse@gmail.com>
2021-02-17 22:38:40 -0600

    Working for creating ec2 instance with configuration

--- Containerization/EC2LambdaCreation.js
+++ Containerization/EC2LambdaCreation.js
@@ -1,54 +1,63 @@
-var AWS = require('aws-sdk');
-var codepipeline = new AWS.CodePipeline();
+var AWS = require('aws-sdk')
+var codepipeline = new AWS.CodePipeline()
 
 exports.handler = async (event) => {
     console.log("Starting EC2 creation...");
-
-    //console.log(JSON.stringify(event, null, 2));
-    //var jobId = event["CodePipeline.job"].id;
-
-    //console.log("Set region...");
-    //AWS.config.update({ region: 'us-east-2' });
-
-    //var params = { jobId: jobId };                      //for exit
-
-    console.log("Setup instance params...");
-    var userData = `#!/bin/bash
-					yum update -y
-					yum install git -y
-					amazon-linux-extras install docker -y
-					service docker start
-					cd home/ec2-user
-					git clone https://github.com/ehelin/TgimbaNetCoreCloudNative.git
-					cd TgimbaNetCore
-					sudo docker build . -t myawesomerepository`;
-    var userDataEncoded = new Buffer(userData).toString('base64');
-    var instanceParams = {
-        ImageId: 'ami-09558250a3419e7d0',
-        InstanceType: 't2.micro',
-        KeyName: 'NetCore3_1KeyPairV2',					// key setup in aws 
-        UserData: userDataEncoded,
-        SecurityGroupIds: ['sg-0f2d4961192b272f3'],		// security group setup in aws
-        MinCount: 1,
-        MaxCount: 1
+    
+    console.log(JSON.stringify(event, null, 2))
+    var jobId = event["CodePipeline.job"].id
+    var params = {
+	jobId: jobId
     };
 
+    console.log("Load credentials and set region from JSON file");
+    AWS.config.update({region: 'us-east-2'});
+
+    var userData = `#!/bin/bash
+		yum update -y
+		yum install git -y
+		amazon-linux-extras install docker -y
+		service docker start
+		cd home/ec2-user
+		git clone https://github.com/ehelin/TgimbaNetCoreCloudNative.git
+		docker build TgimbaNetCoreCloudNative/. -t myawesomerepository`;
+    console.log('User data: ')
+    console.log(JSON.stringify(userData, null, 2))
+
+    var userDataEncoded = Buffer.from(userData).toString('base64');	
+    console.log('User data encoded: ')
+    console.log(JSON.stringify(userDataEncoded, null, 2))
+	
+    var instanceParams = {
+       ImageId: 'ami-09558250a3419e7d0',
+       InstanceType: 't2.micro',
+       KeyName: 'NetCore3_1SampleApp',
+       UserData: userDataEncoded,
+	   SecurityGroupIds: ['sg-0f2d4961192b272f3'],
+       MinCount: 1,
+       MaxCount: 1
+    };
+    console.log('Instance params: ')
+    console.log(JSON.stringify(instanceParams, null, 2))
+    
     console.log("Create a promise on an EC2 service object");
-    var instancePromise = new AWS.EC2({ apiVersion: '2016-11-15' }).runInstances(instanceParams).promise();
-
+    var instancePromise = new AWS.EC2({apiVersion: '2016-11-15'}).runInstances(instanceParams).promise();
+    
     console.log("Handle promise's fulfilled/rejected states");
+    
     return instancePromise.then(
-        function (data) {
-            console.log(data);
-            var instanceId = data.Instances[0].InstanceId;
-            console.log("Created instance", instanceId);
-
-            return 0;//codepipeline.putJobSuccessResult(params).promise();
-        })
-        .catch(
-        function (err) {
-            console.error(err, err.stack);
-
-            return 0;//codepipeline.putJobFailureResult(params).promise();
-        });
-};
+      function(data) {
+    	console.log("inside then");
+        console.log(data);
+        var instanceId = data.Instances[0].InstanceId;
+        console.log("Created instance", instanceId);
+			
+	return codepipeline.putJobSuccessResult(params).promise()
+    }).catch(
+        function(err) {
+    	console.log("inside error");
+        console.error(err, err.stack);
+        
+	return codepipeline.putJobFailureResult(params).promise()
+    });
+}
