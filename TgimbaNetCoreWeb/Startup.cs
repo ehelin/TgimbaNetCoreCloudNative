ea674689d82fa5f2b33c200859d95755c2f3ed64
Eric Helin <ehelin@felpsis.net>
2021-05-10 21:32:19 -0500

    reading secret from aws secret manager

--- TgimbaNetCoreWeb/Startup.cs
+++ TgimbaNetCoreWeb/Startup.cs
@@ -1,8 +1,12 @@
 using System;
+using Amazon;
+using Amazon.SecretsManager;
+using Amazon.SecretsManager.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using TgimbaNetCoreWebShared;
 
 namespace TgimbaNetCoreWeb
@@ -28,15 +32,30 @@ namespace TgimbaNetCoreWeb
 
         private void SetProductionEnvironmentalVariables(IConfiguration Configuration)
         {
-            var dbConn = Configuration.GetSection("DbConnection").Value;
-            var dbConnTest = Configuration.GetSection("DbConnectionTest").Value;
-            string jwtPrivateKey = null;// TODO - get from secret manager
-            var jwtIssuer = Configuration.GetSection("JwtIssuer").Value;
+            var dbConn = Configuration.GetSection("DbConnection")?.Value;
+            var dbConnTest = Configuration.GetSection("DbConnectionTest")?.Value;
+            var jwtIssuer = Configuration.GetSection("JwtIssuer")?.Value;
+
+            dynamic jwtPrivateKeyObj = JsonConvert.DeserializeObject(GetSecret());
+            var jwtPrivateKey = jwtPrivateKeyObj["JwtPrivateKey"]?.ToString();
 
             Environment.SetEnvironmentVariable("DbConnection", dbConn);
             Environment.SetEnvironmentVariable("DbConnectionTest", dbConnTest);
-            Environment.SetEnvironmentVariable("JwtPrivateKey", null);
+            Environment.SetEnvironmentVariable("JwtPrivateKey", jwtPrivateKey);
             Environment.SetEnvironmentVariable("JwtIssuer", jwtIssuer);
+        }
+
+        public string GetSecret()
+        {
+            IAmazonSecretsManager client = new AmazonSecretsManagerClient(RegionEndpoint.GetBySystemName("us-east-2"));
+            GetSecretValueRequest request = new GetSecretValueRequest();
+            request.SecretId = "TgimbaJwtPrivateKey";
+            request.VersionStage = "AWSCURRENT"; 
+
+            var response = client.GetSecretValueAsync(request).Result;
+            var secret = response.SecretString;
+
+            return secret;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
