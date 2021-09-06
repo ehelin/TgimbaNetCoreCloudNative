35a2cdb89ba9f1ea776129a44a318b379dd8219b
Eric Helin <ehelin@felpsis.net>
2021-09-05 23:12:15 -0500

    adding basic welcome app

---
+++ WelcomeApp/Controllers/WelcomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WelcomeApp.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public WelcomeController() {}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
