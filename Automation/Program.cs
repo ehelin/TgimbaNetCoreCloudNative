6c09a2ae1c86d01a4f19d3573ba78f11f8fbfce7
Eric Helin <ehelin@felpsis.net>
2021-09-28 16:34:02 -0500

    adding shell automation

---
+++ Automation/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.AzureAppServices;
+
+namespace Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Console - TgimbaService.cs - ProcessUser(args) - Process is running!");
+            System.Diagnostics.Debug.WriteLine("Debug-TgimbaService.cs - ProcessUser(args) - Process is running!");
+            System.Diagnostics.Trace.WriteLine("Trace-TgimbaService.cs - ProcessUser(args) - Process is running!");
+
+            Console.Read();
+        }
+    }
+}
