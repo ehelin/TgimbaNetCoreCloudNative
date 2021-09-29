5310354519b7817f4d7aa1834a306cc1d05f9dce
Eric Helin <ehelin@felpsis.net>
2021-09-29 13:42:02 -0500

    removed previous nuget packages and added settings schedule

--- Automation/Program.cs
+++ Automation/Program.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging.AzureAppServices;
-
-namespace Automation
+﻿namespace Automation
 {
     class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Console - TgimbaService.cs - ProcessUser(args) - Process is running!");
-            System.Diagnostics.Debug.WriteLine("Debug-TgimbaService.cs - ProcessUser(args) - Process is running!");
-            System.Diagnostics.Trace.WriteLine("Trace-TgimbaService.cs - ProcessUser(args) - Process is running!");
+            System.Console.WriteLine("Console - TgimbaService.cs - ProcessUser(args) - Process is starting!");
 
-            Console.Read();
+            System.Console.WriteLine("Console - TgimbaService.cs - ProcessUser(args) - Process is running!");
+            System.Threading.Thread.Sleep(5000); // simulate some application call
+
+            System.Console.WriteLine("Console - TgimbaService.cs - ProcessUser(args) - Process is stopped!");
         }
     }
 }
